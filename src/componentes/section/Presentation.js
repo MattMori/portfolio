627cfe5ddc038731e38fb9de41f312660a8f3aad
MattMori <Mateus5310@live.com>
2023-07-09 21:44:17 -0300

    Modulo de apresentação concluido

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,39 +1,27 @@
-function Presentation(){
-    return(
-        <div id="Presentation"> <h1>Aqui ficará minha apresentação</h1>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+import styles from './Presentation.module.css'
+import ButtonA from '../elements/ButtonA'
+import ButtonB from '../elements/ButtonB'
+
+function Presentation() {
+    return (
+        <div className={
+                styles.presentation
+            }
+            id="Presentation">
+            <h4>
+                <strong>Bem vindo ao meu portfolio!</strong>
+            </h4>
+            <h1>Olá, Eu sou Mateus Mori!</h1>
+            <p> Sou estudante de Engenharia de Software na Unicesumar<br/>
+                e atualmente cursando a formação em tecnologia na Escola DNC<br/>
+                voltada para o desenvolvimento fullstack web.<br/>
+                Aqui você vai conhecer mais sobre meus conhecimentos<br/>
+                e projetos, fique a vontade para entrar em contato.
+            </p>
+            <ButtonA link="https://github.com/MattMori" text='Conecte-se Comigo'/>
         </div>
-        
+
     )
-}   
+}
 
 export default Presentation
