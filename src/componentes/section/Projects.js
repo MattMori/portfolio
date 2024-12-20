f6b33dabeb94ddeffed6d00219aa47428f4533c3
MattMori <mateus5310@live.com>
2024-12-20 12:50:14 -0300

    Correção de responsividade e ajustes de layout para melhorar a experiência do usuário em telas menores

--- src/componentes/section/Projects.js
+++ src/componentes/section/Projects.js
@@ -5,37 +5,37 @@ import pokedex from '../../image/projects/pokedex.png'
 import escoladnc from '../../image/projects/escoladnc.png'
 import ccxp from '../../image/projects/ccxp.png'
 
-function Projects(){
-    return(
+function Projects() {
+    return (
         <div id="Projects" className={styles.projects}>
-         <h1>Projetos</h1>
-         <Card 
-         img={pokedex}
-         title="Pokedex"
-         tech="JS,CSS,HTML"
-         description="Projeto basico de uma pokedex consumindo a API de pokemon para popular e definir as cores segundo as caracteristicas dos pokemons."
-         repo="https://github.com/MattMori/js-developer-pokedex"
-         site="https://fabulous-clafoutis-334501.netlify.app/"
-         /> 
-         <Card 
-         img={escoladnc}
-         title="Landing page - Escola DNC"
-         tech="JS,CSS,HTML"
-         description="Desenvolvimento de uma landing page da Escola DNC, utilizando-se do figma como base para o lançamento da formação em tecnologia."
-         repo="https://github.com/MattMori/Landind-page-Arquitetura"
-         site="https://storied-klepon-0e060d.netlify.app/"
-         /> 
-         <Card 
-         img={ccxp}
-         title="CCXP 2022"
-         tech="JS,CSS,HTML"
-         description="Um projeto do site da ccxp de 2022 onde possui um cronometro e estilizações CSS e aplicação de video na pagina"
-         repo="https://github.com/MattMori/CCXP"
-         site="https://projetoccxpdnc1.netlify.app/"
-         />
-         <ButtonB text='Acesse meu repositorio' link='https://github.com/MattMori'/>
-         </div>
+            <h1>Projetos</h1>
+            <Card
+                img={pokedex}
+                title="Pokedex"
+                tech="JS,CSS,HTML"
+                description="Projeto de uma pokedex feita em React, utilizando hooks para consumir a API de pokemon para popular os cards, é definido as cores segundo as caracteristicas dos pokemons."
+                repo="https://github.com/MattMori/PokedexReact"
+                site="https://pokedex-react-km7l8sw51-mattmori.vercel.app/"
+            />
+            <Card
+                img={escoladnc}
+                title="Landing page - Escola DNC"
+                tech="JS,CSS,HTML"
+                description="Desenvolvimento de uma landing page da Escola DNC, utilizando-se do figma como base para o lançamento da formação em tecnologia."
+                repo="https://github.com/MattMori/Landind-page-Arquitetura"
+                site="https://storied-klepon-0e060d.netlify.app/"
+            />
+            <Card
+                img={ccxp}
+                title="CCXP 2022"
+                tech="JS,CSS,HTML"
+                description="Um projeto do site da ccxp de 2022 onde possui um cronometro e estilizações CSS e aplicação de video na pagina"
+                repo="https://github.com/MattMori/CCXP"
+                site="https://projetoccxpdnc1.netlify.app/"
+            />
+            <ButtonB text='Acesse meu repositorio' link='https://github.com/MattMori' />
+        </div>
     )
-}   
+}
 
 export default Projects
